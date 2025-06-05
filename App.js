8990a1539f50f7adffc602425f8b49b848733e6f
UmeAimen <168212896+umeaimen337@users.noreply.github.com>
2025-06-05 15:20:44 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CameraScreen from './screens/CameraScreen';
+
+import DisclaimerScreen from './screens/DisclaimerScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Disclaimer">
+        <Stack.Screen name="Disclaimer" component={DisclaimerScreen} 
+        options={{ headerShown: false }}
+        />
+        <Stack.Screen
+  name="CameraScreen"
+  component={CameraScreen}
+  options={{ headerShown: false }}
+/>
+      
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
